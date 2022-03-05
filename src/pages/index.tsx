f068c4589c0bac90cc35b8d7e2e5ea9e4f4bea63
EduardoReisUX <ducabruno09@hotmail.com>
2022-03-05 14:41:09 -0400

    feat: add home page html and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@ import Head from "next/head";
 import type { GetStaticProps } from "next";
 
 import getPopularMovies from "./api/getPopularMovies";
+import Link from "next/link";
+import Image from "next/image";
+import { Pagination } from "../components/Pagination";
 
 interface resultsInterface {
   id: number;
@@ -9,6 +12,7 @@ interface resultsInterface {
   overview: string;
   release_date: string;
   backdrop_path: string | null;
+  poster_path: string | null;
   genre_ids: number[];
 }
 
@@ -22,6 +26,29 @@ type HomeProps = {
 export default function Home({ data }: HomeProps) {
   console.log(data);
 
+  const regex = /(\d+)-(\d+)-(\d+)/;
+  const tags = [
+    "ação",
+    "aventura",
+    "animação",
+    "comédia",
+    "crime",
+    "documentário",
+    "drama",
+    "família",
+    "fantasia",
+    "história",
+    "terror",
+    "música",
+    "mistério",
+    "romance",
+    "ficção científica",
+    "cinema TV",
+    "thriller",
+    "guerra",
+    "faroeste",
+  ];
+
   return (
     <>
       <Head>
@@ -33,11 +60,64 @@ export default function Home({ data }: HomeProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex-1 min-h-screen flex flex-col gap-2 items-center justify-center">
-        <h1 className="text-brand-neutral-900 text-5xl font-bold">
-          Hello, World!
-        </h1>
-        {!!data && data.results[0].title}
+      <main className="min-h-screen bg-brand-primary-dark">
+        <section className="flex flex-col gap-9 text-brand-neutral-000 font-bold px-2 pt-10 pb-12">
+          <h1 className="text-2xl">
+            Milhões de filmes, séries <br />
+            e pessoas para descobrir. <br />
+            Explore já.
+          </h1>
+
+          <section className="flex flex-col gap-2">
+            <h3 className="uppercase text-sm">Filtre por:</h3>
+            <div className="flex flex-wrap gap-3">
+              {tags.map((item) => (
+                <button className="first-letter:uppercase bg-brand-neutral-000 text-brand-neutral-900 text-sm font-bold py-2 px-4 rounded">
+                  {item}
+                </button>
+              ))}
+            </div>
+          </section>
+        </section>
+
+        <section className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-x-4 gap-y-8 px-2 bg-brand-neutral-000 text-brand-neutral-900 font-bold py-8">
+          {data.results.map(
+            ({
+              id,
+              title,
+              release_date,
+              backdrop_path,
+              poster_path,
+              genre_ids,
+            }) => (
+              <Link key={id} href="#" passHref>
+                <a className="flex flex-col gap-2">
+                  <Image
+                    src={`https://image.tmdb.org/t/p/original/${poster_path}`}
+                    alt={title}
+                    width={176}
+                    height={264}
+                    objectFit="cover"
+                    className="rounded"
+                  />
+
+                  <div>
+                    <p className="text-sm">{title}</p>
+                    <small className="text-xs text-brand-neutral-500 uppercase">
+                      {new Date(release_date)
+                        .toLocaleDateString("pt-BR", {
+                          day: "2-digit",
+                          month: "short",
+                          year: "numeric",
+                        })
+                        .replace(/ de |\./g, " ")}
+                    </small>
+                  </div>
+                </a>
+              </Link>
+            )
+          )}
+        </section>
       </main>
     </>
   );

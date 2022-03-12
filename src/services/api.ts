6f28fdced7e4b528ce55f2bd887ced8b6dd665ba
EduardoReisUX <ducabruno09@hotmail.com>
2022-03-12 15:46:09 -0400

    chore: remove axios

--- src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
-  params: {
-    api_key: process.env.ACCESS_TOKEN,
-    language: "pt-BR",
-  },
-});

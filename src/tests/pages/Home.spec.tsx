e6c38d2e346aaf41225e367001ff149cbf6c6472
EduardoReisUX <ducabruno09@hotmail.com>
2022-03-12 14:38:08 -0400

    test: add test for home page getStaticProps

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -1,29 +1,103 @@
-import { render } from "@testing-library/react";
-import Home from "../../pages";
+import { render, screen } from "@testing-library/react";
+import { mocked } from "jest-mock";
+import Home, { getStaticProps } from "../../pages";
+import getPopularMovies from "../../pages/api/getPopularMovies";
+
+jest.mock("../../pages/api/getPopularMovies");
 
 const HomeProps = {
-  page: 1,
-  results: [
-    {
-      id: 1,
-      title: "HomemAranha",
-      overview: "",
-      release_date: "",
-      formattedDate: "",
-      backdrop_path: "",
-      poster_path: "",
-      genre_ids: [1],
-    },
-  ],
+  popularMovies: {
+    page: 1,
+    results: [
+      {
+        id: 1,
+        title: "HomemAranha",
+        overview: "",
+        release_date: "",
+        formattedDate: "",
+        backdrop_path: "",
+        poster_path: "",
+        genre_ids: [1],
+      },
+    ],
+  },
+  genresList: [{ id: 1, name: "test-genre" }],
 };
 
 describe("Home page", () => {
   describe("Receiving mocked HomeProps", () => {
     it("should render a list of heroes", () => {
-      const { getByText } = render(<Home data={HomeProps} />);
+      render(
+        <Home
+          popularMovies={HomeProps.popularMovies}
+          genresList={HomeProps.genresList}
+        />
+      );
 
-      const text = getByText(HomeProps.results[0].title);
+      const text = screen.getByText(HomeProps.popularMovies.results[0].title);
       expect(text).toBeInTheDocument();
+    });
+  });
+
+  describe("getStaticProps", () => {
+    it("should return popularMovies with formatted date, genresList and revalidade of 24hr", async () => {
+      const getPopularMoviesMocked = mocked(getPopularMovies);
+
+      getPopularMoviesMocked.mockResolvedValueOnce({
+        popularMoviesData: {
+          page: 1,
+          results: [
+            {
+              id: 2,
+              title: "Batman",
+              overview: "overview-mock",
+              release_date: "2021-12-24",
+              backdrop_path: "backdrop_path-mock",
+              poster_path: "poster_path-mock",
+              genre_ids: [2],
+            },
+          ],
+        },
+        genresListData: {
+          genres: [
+            {
+              id: 1,
+              name: "ação",
+            },
+          ],
+        },
+      });
+
+      const response = await getStaticProps({});
+
+      expect(response).toEqual(
+        expect.objectContaining({
+          props: {
+            popularMovies: {
+              page: 1,
+              results: [
+                {
+                  id: 2,
+                  title: "Batman",
+                  overview: "overview-mock",
+                  release_date: "2021-12-24",
+                  backdrop_path: "backdrop_path-mock",
+                  poster_path: "poster_path-mock",
+                  genre_ids: [2],
+                  formattedDate: "23 dez  2021",
+                },
+              ],
+            },
+            genresList: [
+              {
+                id: 1,
+                name: "ação",
+              },
+            ],
+          },
+          revalidate: 60 * 60 * 24, // 24 hours
+        })
+      );
     });
   });
 });
